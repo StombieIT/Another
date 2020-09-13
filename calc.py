aae19ccb5eb701c56e733c3abb8918df7cf85cea
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-09-13 16:40:34 +0300

    Update calc.py
    optimized and added new operators

--- calc.py
+++ calc.py
@@ -1,30 +1,48 @@
 def calc(exp: str):
-	if '+' in exp:
-		exp = [calc(sub_exp) for sub_exp in exp.split('+')]
-		result = 0
-		for num in exp:
-			result += num
-		return result
 	if '-' in exp:
-		exp = [calc(sub_exp) for sub_exp in exp.split('-')]
-		result = exp[0]
-		for num in exp[1:]:
-			result -= num
-		return result
-	if '*' in exp:
-		exp = [calc(sub_exp) for sub_exp in exp.split('*')]
-		result = 1
+		exp = (calc(sub_exp) for sub_exp in exp.split('-'))
+		res = next(exp)
 		for num in exp:
-			result *= num
-		return result
+			res -= num
+		del num
+		return res
+	if '+' in exp:
+		exp = (calc(sub_exp) for sub_exp in exp.split('+'))
+		res = 0
+		for num in exp:
+			res += num
+		del num
+		return res
+	if '%' in exp:
+		exp = (calc(sub_exp) for sub_exp in exp.split('%'))
+		res = next(exp)
+		for num in exp:
+			res %= num
+		del num
+		return res
+	if '//' in exp:
+		exp = (calc(sub_exp) for sub_exp in exp.split('//'))
+		res = next(exp)
+		for num in exp:
+			res //= num
+		del num
+		return res
 	if '/' in exp:
-		exp = [calc(sub_exp) for sub_exp in exp.split('/')]
-		result = exp[0]
-		for num in exp[1:]:
-			result /= num
-		return result
+		exp = (calc(sub_exp) for sub_exp in exp.split('/'))
+		res = next(exp)
+		for num in exp:
+			res /= num
+		del num
+		return res
+	if '*' in exp:
+		exp = (calc(sub_exp) for sub_exp in exp.split('*'))
+		res = 1
+		for num in exp:
+			res *= num
+		del num
+		return res
 	if '^' in exp:
-		exp = [calc(sub_exp) for sub_exp in exp.split('^', 1)]
-		result = exp[0] ** exp[1]
-		return result
+		exp = (calc(sub_exp) for sub_exp in exp.split('^', 1))
+		res = next(exp) ** next(exp)
+		return res
 	return int(exp) if exp.isdigit() else 0
