21392eec9537d0216750f7ef185896f26c8b2a03
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-09-13 15:05:06 +0300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,30 @@
+def calc(exp: str):
+	if '+' in exp:
+		exp = [calc(sub_exp) for sub_exp in exp.split('+')]
+		result = 0
+		for num in exp:
+			result += num
+		return result
+	if '-' in exp:
+		exp = [calc(sub_exp) for sub_exp in exp.split('-')]
+		result = exp[0]
+		for num in exp[1:]:
+			result -= num
+		return result
+	if '*' in exp:
+		exp = [calc(sub_exp) for sub_exp in exp.split('*')]
+		result = 1
+		for num in exp:
+			result *= num
+		return result
+	if '/' in exp:
+		exp = [calc(sub_exp) for sub_exp in exp.split('/')]
+		result = exp[0]
+		for num in exp[1:]:
+			result /= num
+		return result
+	if '^' in exp:
+		exp = [calc(sub_exp) for sub_exp in exp.split('^', 1)]
+		result = exp[0] ** exp[1]
+		return result
+	return int(exp)
